e31ff89e0abc8a6ba4e755f783f9e1d832b5e680
soheir96 <sean.oheir1@gmail.com>
2022-08-25 15:33:03 +0100

    solution for exercise 6.5

---
+++ chp6/ex_6_5.py
@@ -0,0 +1,20 @@
+'''The greatest common divisor (GCD) of a and b is the largest number that divides
+both of them with no remainder.
+One way to find the GCD of two numbers is based on the observation that if r is the remainder when
+a is divided by b, then gcd(a, b) = gcd(b, r). As a base case, we can use gcd(a, 0) = a.
+Write a function called gcd that takes parameters a and b and returns their greatest common divisor.'''
+
+def gcd(a, b):
+    '''Finds greatest common divisor for a and b;
+    a, b - integers'''
+    a = int(a)
+    b = int(b)
+
+    # base case 
+    if b == 0:
+        return a
+    
+    return gcd(b, a % b) 
+
+print(gcd(16, 8))
+print(gcd(140, 25))
